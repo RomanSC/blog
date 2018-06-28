8a4d86d4cd3756ff2a40a0efc6560c8eca36a34c
Roman S Collins <r.s.col988@gmail.com>
2018-06-28 01:42:38 +0000

    Start over, I don't know what I'm doing yet

--- hugo-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# hugo --verbose --config="/home/roman/blog/config.toml" -s /home/roman/blog/ -d /var/www/public/hugo
-sudo hugo --verbose --config="/home/roman/blog/config.toml" -s /home/roman/blog/ -d /var/www/public/hugo -l /home/roman/blog/layouts -t .
